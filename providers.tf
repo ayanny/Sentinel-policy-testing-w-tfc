df1263635bebe5fb737324baf06b2ec9ed5553bf
Amir Yanny <ayanny@users.noreply.github.com>
2022-12-01 18:21:44 -0700

    First Run

--- providers.tf
+++ providers.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = ">=4.37.0"
+      source  = "hashicorp/aws"
+      version = ">= 4.37.0"
     }
-    cloudinit {
-        source = "hashicorp/cloudinit"
-        version = ">=2.2.0"
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = ">= 2.2.0"
     }
     tls = {
-        source = "hashicorp/tls"
-        version = ">=4.0.3"
+      source  = "hashicorp/tls"
+      version = ">= 4.0.3"
     }
   }
 }
